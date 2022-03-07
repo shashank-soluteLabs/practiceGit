9c75d0d0bbf26953cc25a3069d01c7810deb9ad5
shashank0194 <shashank0194@gmail.com>
2022-03-07 23:25:03 +0530

    temperature calci with class calculator

--- revisionReact/revision/src/App.js
+++ revisionReact/revision/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { Clock } from './components/Clock';
+import { Toggle } from './components/Toggle';
+import { LoginControl } from './components/LoginControl';
+import { Calculator } from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <Clock/>
+      <hr/>
+      <Toggle/>
+      <hr/>
+      <LoginControl/>
+      <hr/>
+      <Calculator/>
     </div>
   );
 }
